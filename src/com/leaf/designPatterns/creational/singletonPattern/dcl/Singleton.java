df26c5b687765a2cd8eade1726c836acffb3bc70
leshu <leshu@chinaums.com>
2025-03-10 16:14:55 +0800

    单例模式

---
+++ src/com/leaf/designPatterns/creational/singletonPattern/dcl/Singleton.java
@@ -0,0 +1,36 @@
+package com.leaf.designPatterns.creational.singletonPattern.dcl;
+
+/**
+ * 双检锁/双重校验锁（DCL，即 double-checked locking）
+ * JDK 版本：JDK1.5 起
+ * <p>
+ * 是否 Lazy 初始化：是
+ * <p>
+ * 是否多线程安全：是
+ * <p>
+ * 实现难度：较复杂
+ * <p>
+ * 描述：这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
+ * getInstance() 的性能对应用程序很关键。
+ *
+ * @author leshu
+ * @since 2025/3/10 16:02
+ **/
+public class Singleton {
+
+    private volatile static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
